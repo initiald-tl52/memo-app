4884980c448eb4261c731b69632e859bf7394199
いとうだいち <itoudaichi@itoudaichinoMacBook-Pro.local>
2019-09-30 14:02:39 +0900

    git start

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,18 +1,2 @@
 class TodosController < ApplicationController
-  class TodosController < ApplicationController
-    def index
-      @todo = Todo.new
-      @todos = Todo.order('created_at ASC')
-    end
-  
-    def create
-      Todo.create(todo_params)
-      redirect_to :root
-    end
-  
-    private
-    def todo_params
-      params.require(:todo).permit(:content)
-    end
-  end
 end
